ba80cdeed7627a11395b2fe6a5041c23a5d475e8
henchir <elhenchirmohamed@gmail.com>
2018-11-30 22:42:05 +0100

    comment

--- src/main/java/example/Person.java
+++ src/main/java/example/Person.java
@@ -1,28 +1,51 @@
 package example;
 
+/**
+ * @author mhenchir
+ *
+ */
 public class Person {
 
+	/**
+	 * 
+	 */
 	private String firstName;
 	private String lastName;
 
+	/**
+	 * @param firstName
+	 * @param lastName
+	 */
 	public Person(String firstName, String lastName) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * @param firstName
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * @param lastName
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
