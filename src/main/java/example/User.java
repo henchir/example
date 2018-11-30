5947496a7effd9bf064d2c48399d3236cc34dd16
henchir <elhenchirmohamed@gmail.com>
2018-11-30 22:24:35 +0100

    f2

--- src/main/java/example/User.java
+++ src/main/java/example/User.java
@@ -1,5 +1,29 @@
 package example;
 
-public class User {
+public class User extends Person {
 
+	private String userName;
+	private String password;
+
+	public User(String firstName, String lastName, String userNamer, String password) {
+		super(firstName, lastName);
+		this.userName = userNamer;
+		this.password = password;
+	}
+
+	public String getUserNamer() {
+		return userName;
+	}
+
+	public void setUserNamer(String userNamer) {
+		this.userName = userNamer;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
